fcf97c93496d85761d9b10c8146e5175013b3312
shivam misra <shivam.misra@>
2018-09-29 18:31:10 +0530

    Initial commit

---
+++ src/main/java/org/springframework/gsmysqldata/GsMysqlDataApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.gsmysqldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsMysqlDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsMysqlDataApplication.class, args);
+	}
+}
