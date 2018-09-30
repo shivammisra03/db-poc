03eb3b88a48f043a48f6302b6b790fe051ae6cf8
shivam misra <shivam.misra@>
2018-09-30 14:59:04 +0530

    added get method to fetch all users

--- src/main/java/org/springframework/gsmysqldata/controller/UserController.java
+++ src/main/java/org/springframework/gsmysqldata/controller/UserController.java
@@ -1,8 +1,12 @@
 package org.springframework.gsmysqldata.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.gsmysqldata.entity.User;
 import org.springframework.gsmysqldata.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,6 +23,14 @@ public class UserController {
 	public String save(@RequestBody User user) {
 		userRepository.save(user);
 		return "saved";
+
+	}
+
+	@GetMapping(value = "/getAllUsers")
+	public List<User> getAllUsers() {
+		List<User> list = new ArrayList<>();
+		userRepository.findAll().forEach(user -> list.add(user));
+		return list;
 
 	}
 
